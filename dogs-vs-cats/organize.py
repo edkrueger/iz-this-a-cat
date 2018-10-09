0fb50ebc528636d4d7d7f989b3b389cd9faeadaf
Edward Krueger <edkrueger@gmail.com>
2018-10-09 11:12:01 -0500

    Update the organizer to split the images into test and train sets.

--- dogs-vs-cats/organize.py
+++ dogs-vs-cats/organize.py
@@ -1,28 +1,45 @@
 import os
 import shutil
+from numpy.random import seed, shuffle
+from numpy import floor
 
-path_to_train_data = os.path.join("unorganized_data", "train")
+path_to_labeled_data = os.path.join("unorganized_data", "train")
 
-def organize_train_data(in_dir, out_dir):
-    
-    filenames = os.listdir(in_dir)
-    
-    try:
-        os.mkdir(out_dir)
-    except:
-        pass
+def organize_data(in_dir, out_dir, train_size, set_seed=1000):
 
-    for filename in filenames:
+	def move_images(filenames, in_dir, destination):
 
-        label = filename.split(".")[0]
+		for filename in filenames:
 
-        try:
-            os.mkdir(os.path.join(out_dir, label))
-        except:
-            pass
+			label = filename.split(".")[0]
 
-        path_to_image = os.path.join(in_dir, filename)
-        
-        shutil.copy(path_to_image, os.path.join(out_dir, label, filename))
+			os.makedirs(os.path.join(destination, label), exist_ok=True)
 
-organize_train_data(path_to_train_data, "data")
+			path_to_image = os.path.join(in_dir, filename)
+
+			shutil.copy(path_to_image, os.path.join(destination, label, filename))
+
+		return
+
+	seed(set_seed)
+
+	filenames = os.listdir(in_dir)
+	shuffle(filenames)
+
+	train_cutoff = int(floor(len(filenames) * train_size))
+	train_filenames = filenames[0:train_cutoff]
+	test_filenames = filenames[train_cutoff:]
+
+	train_dir = os.path.join(out_dir, "train")
+	test_dir = os.path.join(out_dir, "test")
+
+
+	os.makedirs(train_dir, exist_ok=True)
+	os.makedirs(test_dir, exist_ok=True)
+
+	move_images(train_filenames, in_dir, train_dir)
+	move_images(test_filenames, in_dir, test_dir)
+
+	return
+
+organize_data(path_to_labeled_data, "data", .8)
